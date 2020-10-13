f61ee0478434e74a1219b96e0882f1aec0f7cf04
Elias Amha <mr.m.r.s.h.i.e.l.d@gmail.com>
2020-10-13 15:53:22 +0300

    Update doer.sh

--- .github/actions/cat-facts/doer.sh
+++ .github/actions/cat-facts/doer.sh
@@ -1,19 +1,13 @@
+echo $GIST_TOKEN > ~/.gist
 cd /all;
-chmod +x gist.sh
 for mdmn in `cat master_list`;do
     mdmnp=`echo "$mdmn" | tr ':/*.' '_'`; 
     mkdir results_$mdmnp;
-    python3 ofa/oneforall.py run --brute False --target $mdmn --path results_$mdmnp;
+    python3 ofa/oneforall.py run --takeover True --brute False --target $mdmn --path results_$mdmnp;
     RESPATH=results_$mdmnp;
     mv $RESPATH/all*.txt $RESPATH/init;
-    ./gist.sh $RESPATH/init $GIST_TOKEN
-    mkdir $RESPATH/nikto;
-    for i in `cat $RESPATH/init`; do
-     j=`echo "$i" | tr ':/*.' '_'`; 
-     touch $RESPATH/nikto/scan_$j.txt; 
-     nikto -Display V -host $i -port 80 -o $RESPATH/nikto/scan_$j.txt;
-     nikto -Display V -host $i -port 443 -o $RESPATH/nikto/scan_$j_443.txt;
-    done
+    echo '--------\n' >> $RESPATH/init;
+    gist-paste $RESPATH/init -f "$mdmn"__`date +%s`    
 done
-
-zip -r final.zip results_*
+cat /all/ofa/results/takeover* > takeovers
+gist-paste takeovers -f "$mdmn"_"takeovers"_`date +%s`    
