a7d1028eb55cd3e0a85e23acf5cc59ea839f4c02
Elias Amha <mr.m.r.s.h.i.e.l.d@gmail.com>
2020-11-02 12:34:58 +0300

    Update doer.sh

--- .github/actions/cat-facts/doer.sh
+++ .github/actions/cat-facts/doer.sh
@@ -2,7 +2,7 @@ echo $GIST_TOKEN > ~/.gist
 touch /all/master
 cp telegram.sh /all/telegram.sh
 cd /all;
-chmod +x telegram.sh
+chmod +x /all/telegram.sh
 for mdmn in `cat master_list`;do
     mdmnp=`echo "$mdmn" | tr ':/*.' '_'`; 
     mkdir results_$mdmnp;
@@ -11,7 +11,7 @@ for mdmn in `cat master_list`;do
     mv $RESPATH/all*.txt $RESPATH/init;
     gist-paste $RESPATH/init -f "$mdmn"__`date +%s`    
     cat $RESPATH/init >> /all/master
-    cat $RESPATH/init | python3 /all/Shutter/shutter.py --stdin | xargs -n1 -I{} telegram.sh -i {} -t $ME_TOKEN -c 303757027
+    cat $RESPATH/init | python3 /all/Shutter/shutter.py --stdin | xargs -n1 -I{} /all/telegram.sh -i {} -t $ME_TOKEN -c 303757027
 done
 cat /all/ofa/results/takeover* > takeovers
 python3 gag.py /all/opt
