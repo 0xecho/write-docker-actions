33e4363cae5c8e0a86066ff77125500dc7d9211d
Elias Amha <mr.m.r.s.h.i.e.l.d@gmail.com>
2020-11-02 12:14:59 +0300

    Update doer.sh

--- .github/actions/cat-facts/doer.sh
+++ .github/actions/cat-facts/doer.sh
@@ -1,20 +1,20 @@
 echo $GIST_TOKEN > ~/.gist
 touch /all/master
 cd /all;
+chmod +x telegram.sh
 for mdmn in `cat master_list`;do
     mdmnp=`echo "$mdmn" | tr ':/*.' '_'`; 
     mkdir results_$mdmnp;
     python3 ofa/oneforall.py run --takeover True --brute False --target $mdmn --path results_$mdmnp;
     RESPATH=results_$mdmnp;
     mv $RESPATH/all*.txt $RESPATH/init;
-    echo '--------\n' >> $RESPATH/init;
     gist-paste $RESPATH/init -f "$mdmn"__`date +%s`    
-    cat $RESPATH/init > /all/master
+    cat $RESPATH/init >> /all/master
+    cat $RESPATH/init | python3 /all/Shutter/shutter.py --stdin | xargs -n1 -I{} telegram.sh -i {} -t $ME_TOKEN -c 303757027
 done
 cat /all/ofa/results/takeover* > takeovers
 python3 gag.py /all/opt
 comm -13 <(sort < /all/opt) <(sort < /all/master) > /all/final
 gist-paste /all/final -f `date +%s`    
 gist-paste takeovers -f "takeovers"_`date +%s`    
-chmod +x telegram.sh
-cat /all/opt | python3 /all/Shutter/shutter.py --stdin | xargs -n1 -I{} telegram.sh -i {} -t $ME_TOKEN -c 303757027
+
