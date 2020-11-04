41d07efdbb9b1fe67a172fefb47566f1edc81dfe
Elias Amha <mr.m.r.s.h.i.e.l.d@gmail.com>
2020-11-04 15:53:47 +0300

    Update doer.sh

--- .github/actions/cat-facts/doer.sh
+++ .github/actions/cat-facts/doer.sh
@@ -2,5 +2,5 @@ cd /all
 ls
 pwd
 echo $GIST_TOKEN > ~/.gist
-cat resolve.txt | tee >(gau) | xargs -n1 -I{} -P 10 gospider -d 3 -q -s {} | rev | cut -d' ' -f1 | rev | grep 'oppo.com' | awk '!seen[$0]++'| xargs -n1 -P 10 -I {} bash -c 'echo {} | httpx -silent' > opt
-gist-paste opt -f "Oppo Endpoints"
+cat oppo | python3 /all/Arjun/arjun.py --silent | tee opt
+gist-paste opt -f 'with_params'
