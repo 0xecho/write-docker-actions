cff44e03b3ac2c411a4aba6541bb4e50bc1179f6
Elias Amha <mr.m.r.s.h.i.e.l.d@gmail.com>
2020-11-04 15:14:18 +0300

    Update doer.sh

--- .github/actions/cat-facts/doer.sh
+++ .github/actions/cat-facts/doer.sh
@@ -1,4 +1,6 @@
 cd /all
 ls
 pwd
-cat resolve.txt | tee >(gau) >(xargs -I{} gospider -d 3 -q -s {} | rev | cut -d' ' -f1 | rev) | awk '!seen[$0]++'| xargs -n1 -P 10 -I {} bash -c 'echo {} | httpx -silent' | kxss
+echo $GIST_TOKEN > ~/.gist
+cat resolve.txt | tee >(gau) >(xargs -I{} gospider -d 3 -q -s {} | rev | cut -d' ' -f1 | rev) | awk '!seen[$0]++'| xargs -n1 -P 10 -I {} bash -c 'echo {} | httpx -silent' >> opt
+gist-paste opt -f "Oppo Endpoints"
