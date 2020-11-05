942be1d75ce3eb5be4ff60defdee39d64c87146a
Elias Amha <mr.m.r.s.h.i.e.l.d@gmail.com>
2020-11-05 13:12:00 +0300

    Update doer.sh

--- .github/actions/cat-facts/doer.sh
+++ .github/actions/cat-facts/doer.sh
@@ -2,5 +2,4 @@ ls
 pwd
 cd /all
 echo $GIST_TOKEN > ~/.gist
-cat resolve.txt | tee >(gau) | xargs -n1 -I{} -P 20 gospider -d 10 -t 100 -q -s {} | rev | cut -d' ' -f1 | rev | grep 'oppo.com' |grep -v '.js|.jpg|.jpeg|.gif|.css|.tif|.tiff|.png|.ttf|.woff|.woff2|.ico|.pdf|.svg|.txt' | awk '!seen[$0]++'| xargs -n1 -P 10 -I {} bash -c 'echo {} | httpx -silent' > opt
-gist-paste opt -f "Oppo Endpoints V2.0"
+cat oppo | xargs -n1 -P 10 -I {} bash -c 'FL=`echo {}|tr '\/' '_'`;python3 /all/dirsearch/dirsearch.py -u {} -E --plain-text-report=$FL; gist-paste $FL -f "{} DIRSEARCHED"'
