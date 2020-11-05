af437a3e7790502bc4b22c6aff8e7debb7039da4
Elias Amha <mr.m.r.s.h.i.e.l.d@gmail.com>
2020-11-05 15:19:19 +0300

    Update doer.sh

--- .github/actions/cat-facts/doer.sh
+++ .github/actions/cat-facts/doer.sh
@@ -2,4 +2,5 @@ ls
 pwd
 cd /all
 echo $GIST_TOKEN > ~/.gist
-cat oppo | xargs -n1 -P 10 -I {} bash -c 'FL=`echo {}|tr '\/' '_'`;python3 /all/dirsearch/dirsearch.py -u {} -E --plain-text-report=$FL; gist-paste $FL -f "{} DIRSEARCHED"'
+#cat oppo | xargs -n1 -P 10 -I {} bash -c 'FL=`echo {}|tr '\/' '_'`;python3 /all/dirsearch/dirsearch.py -u {} -E --plain-text-report=$FL; gist-paste $FL -f "{} DIRSEARCHED"'
+cat urls | xargs -n1 -P 10 -I {} bash -c ' python3 /all/ps/paramspider.py --domain {} --level high --silent >/dev/null; cat output/{}.txt' | xargs -n1 -P10 -I% bash -c 'dalfox url % --silence'
