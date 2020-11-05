9b93d8bde2f7e75aff8544ec4da21f2f2ba35222
Elias Amha <mr.m.r.s.h.i.e.l.d@gmail.com>
2020-11-05 17:27:09 +0300

    Update doer.sh

--- .github/actions/cat-facts/doer.sh
+++ .github/actions/cat-facts/doer.sh
@@ -3,4 +3,4 @@ pwd
 cd /all
 echo $GIST_TOKEN > ~/.gist
 #cat oppo | xargs -n1 -P 10 -I {} bash -c 'FL=`echo {}|tr '\/' '_'`;python3 /all/dirsearch/dirsearch.py -u {} -E --plain-text-report=$FL; gist-paste $FL -f "{} DIRSEARCHED"'
-cat urls | xargs -n1 -P 10 -I {} bash -c 'python3 /all/ps/paramspider.py --domain https://{} --level high' | xargs -n1 -P10 -I% bash -c "dalfox url '%' --silence --follow-redirects 2>/dev/null"
+cat urls | xargs -n1 -P 10 -I {} bash -c 'python3 /all/ps/paramspider.py --domain https://{} --level high' | xargs -n1 -P10 -I% bash -c "dalfox url '%' --silence --follow-redirects 2>/dev/null" | xargs -P 30 -n1 -I{} /all/telegram.sh -t $ME_TOKEN -c 303757027 -T {}
